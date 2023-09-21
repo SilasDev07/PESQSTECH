04e1e9fd7f198c669ae1fa976899f8606b67d090
silaaasxz1 <143753926+SilasDev07@users.noreply.github.com>
2023-09-21 17:42:30 -0300

    Add files via upload

---
+++ QSTECH/script.js
@@ -0,0 +1,19 @@
+function abrirMenu() {
+    if (itens.style.display == 'none') {
+        itens.style.display = 'block'
+   
+    }
+
+
+
+
+    else {
+        itens.style.display = 'none'
+    }
+
+
+
+
+}
+
+
